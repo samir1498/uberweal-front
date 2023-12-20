8980bb2ce0bb05b0e7889aab5b1ef215e98a36d3
Samir1498 <samirsbx2@gmail.com>
2023-12-20 15:44:24 +0100

    implementing book ride use case

---
+++ src/application/bookRideUseCase.ts
@@ -0,0 +1,32 @@
+// application/BookRideUseCase.ts
+
+import { RideRepository } from "../domain/repositories/RideRepository";
+import { Ride, RideStatus, Location } from "../domain/entities/Ride";
+import { Rider } from "../domain/entities/Rider";
+
+export class BookRideUseCase {
+  private rideRepository: RideRepository;
+
+  constructor(rideRepository: RideRepository) {
+    this.rideRepository = rideRepository;
+  }
+
+  async bookRide(
+    rider: Rider,
+    startLocation: Location,
+    endLocation: Location
+  ): Promise<number> {
+
+    const ride: Omit<Ride, "id"> = {
+      rider: rider,
+      status: RideStatus.IN_PROGRESS,
+      startLocation,
+      endLocation,
+    };
+
+    const rideId: number = await this.rideRepository.book(ride);
+
+
+    return rideId;
+  }
+}
